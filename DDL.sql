655944b6f4ed7c6327d71b533de0f0b26fde8786
Rajvi Dave <116886200+RajviDave@users.noreply.github.com>
2026-06-19 23:50:26 +0530

    DDL query

---
+++ DDL.sql
@@ -0,0 +1,17 @@
+USE MyDatabase;
+
+/*CREATE TABLE users(
+id INT NOT NULL,
+email VARCHAR(30) PRIMARY KEY,
+user_name VARCHAR(30),
+);*/
+
+/*ALTER TABLE users
+	--ADD phone INT NOT NULL,
+	DROP COLUMN user_name;*/
+
+/*ALTER TABLE users
+	ADD user_name VARCHAR(30);*/
+
+DROP TABLE users;
+
